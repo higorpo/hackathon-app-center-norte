57f980d47c6f53365744b9fc5ae0ec654140f5f1
higorpo <higorpiresoliveira@gmail.com>
2024-07-13 11:11:27 -0300

    ajusta problemas

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,11 +7,11 @@ import { ThemedView } from '@/components/ThemedView';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: 'Teste!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+        <ThemedText type="title">Este app é um protótipo de coleta de geolocalização.</ThemedText>
+        <Link href="/map" style={styles.link}>
+          <ThemedText type="link">Ir para tela do teste!</ThemedText>
         </Link>
       </ThemedView>
     </>
